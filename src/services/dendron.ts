d9b1357e6dd7b0a15b255bd1b8bee657c51ca2ef
Kevin <kevin@dendron.so>
2023-04-29 20:16:09 -0700

    add markdoc integration

--- src/services/dendron.ts
+++ src/services/dendron.ts
@@ -1,9 +1,14 @@
 // this `FernApi` export is generated from your organization name in fern.config.json:
 import { FernApi } from "../api";
 import { DendronService } from "../api/generated/api/resources/dendron/service/DendronService";
+import Markdoc from '@markdoc/markdoc';
 
 export default new DendronService({
   markdownRender(req, res) {
-    return res.send({ content: "BOND" });
+    const source = req.body.content
+    const ast = Markdoc.parse(source);
+    const content = Markdoc.transform(ast, /* config */);
+    const html = Markdoc.renderers.html(content);
+    return res.send({ content: html });
   }
 });
