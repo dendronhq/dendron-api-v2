aadd2412a1f1b52f5d08354472b43934db93ad29
Kevin <kevin@dendron.so>
2023-05-04 19:25:04 -0700

    fix type issues

--- src/serviceImpl/sync/syncToService.ts
+++ src/serviceImpl/sync/syncToService.ts
@@ -1,62 +1,64 @@
 import fs from "fs-extra";
-import matter from 'gray-matter';
-import minimatch from 'minimatch';
+import matter from "gray-matter";
+import _ from "lodash";
+import { minimatch } from "minimatch";
+import * as z from "zod";
 import { SyncToRequest } from "../../api/generated/api";
-import { MarkdownDestination } from "./markdownDestination";
-import * as z from 'zod';
-import _ from "lodash"
 import { logger } from "../../logger";
-
+import { MarkdownDestination } from "./markdownDestination";
 
 type IncludeOption = {
-  hierarchies: string[]
-}
+  hierarchies: string[];
+};
 
 type ExcludeOption = {
-  tags: { key: string, value: any }[]
-  hierarchies: string[]
-}
-
+  tags: { key: string; value: any }[];
+  hierarchies: string[];
+};
 
 const processIncludeOption = (value: string): IncludeOption => {
-  const out = value.split(',').reduce((acc: Partial<IncludeOption>, param: string) => {
-    const [key, value] = param.split('=');
-    let cvalue: any = value;
-    switch (key) {
-      case 'hierarchies':
-        cvalue = value.split("|");
-        acc[key] = cvalue;
-        break;
-      default:
-        throw new Error(`Invalid key "${key}"`);
-    }
-    return acc;
-
-  }, {});
+  const out = value
+    .split(",")
+    .reduce((acc: Partial<IncludeOption>, param: string) => {
+      const [key, value] = param.split("=");
+      let cvalue: any = value;
+      switch (key) {
+        case "hierarchies":
+          cvalue = value.split("|");
+          acc[key] = cvalue;
+          break;
+        default:
+          throw new Error(`Invalid key "${key}"`);
+      }
+      return acc;
+    }, {});
   return _.defaults(out, { hierarchies: ["*"] });
-}
+};
 
 const processExcludeOption = (value: string): ExcludeOption => {
-  const out = value.split(',').reduce((acc: Partial<ExcludeOption>, param: string) => {
-    const [key, value] = param.split('=');
-    let cvalue: any = value;
-    switch (key) {
-      case 'tags':
-        const [tagKey, tagValue] = value.split(':');
-        cvalue = [{ key: tagKey, tagValue }]
-        acc[key] = cvalue;
-        break;
-      case 'hierarchies':
-        cvalue = value.split("|");
-        acc[key] = cvalue;
-        break;
-      default:
-        throw new Error(`Invalid key "${key}"`);
-    }
-    return acc;
-  }, {});
+  const out = value
+    .split(",")
+    .reduce((acc: Partial<ExcludeOption>, param: string) => {
+      const [key, value] = param.split("=");
+      let cvalue: any = value;
+      switch (key) {
+        case "tags": {
+          const [tagKey, tagValue] = value.split(":");
+          cvalue = [{ key: tagKey, tagValue }];
+          acc[key] = cvalue;
+          break;
+        }
+        case "hierarchies":
+          cvalue = value.split("|");
+          acc[key] = cvalue;
+          break;
+        default:
+          throw new Error(`Invalid key "${key}"`);
+      }
+      return acc;
+    }, {});
   return _.defaults(out, { tags: [], hierarchies: [] });
-}
+};
 
 const optionsSchema = z.object({
   include: z.string().transform(processIncludeOption),
@@ -64,23 +66,22 @@ const optionsSchema = z.object({
   targetFormat: z.string(),
   src: z.string(),
   dest: z.string(),
-  deleteMissing: z.boolean().default(false)
+  deleteMissing: z.boolean().default(false),
 });
 
-
 export class SyncToService {
-
   async execute(args: SyncToRequest) {
     const ctx = "SyncToService";
     const _args = optionsSchema.parse(args);
     logger.info({ ctx, msg: "enter", args: _args });
 
     // Read all files with frontmatter using the "gray-matter" library
-    let files = fs.readdirSync(args.src)
-      .filter(file => file.endsWith('.md'))
-      .map(file => {
-        const contents = fs.readFileSync(`${args.src}/${file}`, 'utf8');
-        const fname = file.replace(/\.md$/, '');
+    const files = fs
+      .readdirSync(args.src)
+      .filter((file) => file.endsWith(".md"))
+      .map((file) => {
+        const contents = fs.readFileSync(`${args.src}/${file}`, "utf8");
+        const fname = file.replace(/\.md$/, "");
         return { ...matter(contents), fname, fpath: `${args.src}/${file}` };
       });
     logger.info({ ctx, msg: "fin:readFiles", numFiles: files.length });
@@ -93,15 +94,23 @@ export class SyncToService {
 
     // include
     const hierarchies = _args.include.hierarchies;
-    let filesToSync = hierarchies.flatMap(hierarchyMatchPattern => {
-      return files.filter(file => minimatch(file.fname, hierarchyMatchPattern));
-    }) as unknown as (typeof files)
-    logger.info({ ctx, msg: "fin:filterHierarchies", numFiles: filesToSync.length });
+    let filesToSync = hierarchies.flatMap((hierarchyMatchPattern) => {
+      return files.filter((file) =>
+        minimatch(file.fname, hierarchyMatchPattern)
+      );
+    }) as unknown as typeof files;
+    logger.info({
+      ctx,
+      msg: "fin:filterHierarchies",
+      numFiles: filesToSync.length,
+    });
 
     // exclude
-    filesToSync = _args.exclude.hierarchies.flatMap(hierarchyMatchPattern => {
-      return filesToSync.filter(file => !minimatch(file.fname, hierarchyMatchPattern));
-    }) as unknown as (typeof files)
+    filesToSync = _args.exclude.hierarchies.flatMap((hierarchyMatchPattern) => {
+      return filesToSync.filter(
+        (file) => !minimatch(file.fname, hierarchyMatchPattern)
+      );
+    }) as unknown as typeof files;
     // _args.exclude.tags.forEach(tag => {
     //   filesToSync = filesToSync.filter(file => file.data[tag.key] !== tag.value);
     // });
@@ -109,22 +118,26 @@ export class SyncToService {
     // filesToSync = filesToSync.filter(file => (!('public' in file.data) || file.data['public']));
     logger.info({ ctx, msg: "fin:excludeTags", numFiles: filesToSync.length });
 
-    logger.info({ctx, matches: filesToSync.map(file => file.fname).join('\n')});
+    logger.info({
+      ctx,
+      matches: filesToSync.map((file) => file.fname).join("\n"),
+    });
     // TODO: support incremental sync
 
     if (_args.deleteMissing) {
-      logger.info({ ctx, msg: "deleteMissing", dest: _args.dest })
-      fs.emptyDirSync(_args.dest)
+      logger.info({ ctx, msg: "deleteMissing", dest: _args.dest });
+      fs.emptyDirSync(_args.dest);
     }
 
     switch (args.targetFormat) {
-      case 'markdown':
-        const dest = new MarkdownDestination()
-        return dest.sync(filesToSync, args)
+      case "markdown": {
+        const dest = new MarkdownDestination();
+        return dest.sync(filesToSync, args);
+      }
       default:
-        throw new Error(`Error: "${args.targetFormat}" is not a supported exclude parameter.`);
+        throw new Error(
+          `Error: "${args.targetFormat}" is not a supported exclude parameter.`
+        );
     }
-
-  };
-
+  }
 }
